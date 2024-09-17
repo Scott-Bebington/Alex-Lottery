410ea4f6a2944c300dbbef88b3c058972d082e11
Scott-Bebington <133017388+Scott-Bebington@users.noreply.github.com>
2024-09-17 17:34:57 +0200

    Added profile page

---
+++ src/app/pages/profile.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { use, useEffect, useRef, useState } from 'react';
+
+export default function Profile() {
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
